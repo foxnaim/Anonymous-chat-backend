f0de3064166cd888572059150c44a9f9947b746a
Rick <118506963+foxnaim@users.noreply.github.com>
2025-12-16 14:42:59 +0500

    chore: удален неиспользуемый код из валидатора пароля

--- src/validators/authValidator.ts
+++ src/validators/authValidator.ts
@@ -1,27 +1,5 @@
 import { z } from 'zod';
 
-// Валидация надежности пароля
-const strongPasswordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+\-=\[\]{}|;:,.<>?]).{8,}$/;
-
-const passwordRefinement = (password: string) => {
-  if (password.length < 8) {
-    return { success: false, message: 'Password must be at least 8 characters long' };
-  }
-  if (!/[A-Z]/.test(password)) {
-    return { success: false, message: 'Password must contain at least one uppercase letter' };
-  }
-  if (!/[a-z]/.test(password)) {
-    return { success: false, message: 'Password must contain at least one lowercase letter' };
-  }
-  if (!/[0-9]/.test(password)) {
-    return { success: false, message: 'Password must contain at least one digit' };
-  }
-  if (!/[!@#$%^&*()_+\-=\[\]{}|;:,.<>?]/.test(password)) {
-    return { success: false, message: 'Password must contain at least one special character (!@#$%^&*()_+-=[]{}|;:,.<>?)' };
-  }
-  return { success: true };
-};
-
 export const loginSchema = z.object({
   body: z.object({
     email: z.string().email('Invalid email format'),
